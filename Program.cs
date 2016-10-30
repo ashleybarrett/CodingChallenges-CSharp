933294a934f1ba63174933ac49b03d4f545a4224
ashleybarrett <ashleybarrett.mail@googlemail.com>
2016-10-30 16:47:20 +0000

    removed log message

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace ConsoleApplication
+﻿namespace ConsoleApplication
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
