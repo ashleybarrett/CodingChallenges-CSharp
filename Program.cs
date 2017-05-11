48a5ae07fee4bac9e2788f178c7907572c448e8d
Ashley Barrett <ashleybarrett@Ashleys-MacBook-Pro.local>
2017-05-11 10:54:28 +0100

    removed main body

--- Program.cs
+++ Program.cs
@@ -7,15 +7,7 @@ namespace CodingChallenges_CSharp
     {
         public static void Main(string[] args)
         {
-            var tests = BallSwitchBoardTests.GetTests();
-            var sut = new BallSwitchBoardTests();
-            foreach (var test in tests)
-            {
-                if ((int)test[0] == 6)
-                {
-                    sut.RunTests((int)test[0], (int[][])test[1], (int)test[2], (int)test[3]);       
-                } 
-            }
+            
         }
     }
 }
